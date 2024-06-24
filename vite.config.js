fdaa69f4931f9113eeb444bf84aa56aed44aea68
louiesun <95512329+louiesun@users.noreply.github.com>
2024-06-24 11:11:38 +0800

    rewrite with vite

---
+++ vite.config.js
@@ -0,0 +1,31 @@
+import {defineConfig} from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+
+export default defineConfig({
+    plugins: [
+        VitePWA({
+            injectRegister: 'auto',
+            registerType: 'autoUpdate',
+            devOptions: {
+                enabled: true
+            },
+            includeAssets: ['favicon.svg'],
+            manifest: {
+                name: 'Cryption Aganst Censorship',
+                short_name: "Crypted",
+                theme_color: "#39c5bb",
+                start_url: "./",
+                display: "standalone",
+                background_color: "#39c5bb",
+                icons: [
+                    {
+                        src: "favicon.svg",
+                        sizes: "16x16 32x32 48x48 64x64 128x128 256x256 512x512",
+                        type: "image/svg+xml",
+                        purpose: "any",
+                    },
+                ],
+            },
+        })
+    ],
+});
