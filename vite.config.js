4f675cc3f7cbde6b0c85ab961755edbf522b0f6f
louiesun <95512329+louiesun@users.noreply.github.com>
2024-06-24 13:53:12 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import {defineConfig} from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-    base: '/Crypted/',
+    base: '/Crypted',
     plugins: [
         VitePWA({
             injectRegister: 'auto',
