078e39c4eeb7f6ecf4e603320663e50b62ad9b9d
louiesun <95512329+louiesun@users.noreply.github.com>
2024-06-24 13:22:35 +0800

    fix bug

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import {defineConfig} from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-    base: 'Crypted',
+    base: '/Crypted/',
     plugins: [
         VitePWA({
             injectRegister: 'auto',
@@ -15,7 +15,7 @@ export default defineConfig({
                 name: 'Cryption Aganst Censorship',
                 short_name: "Crypted",
                 theme_color: "#39c5bb",
-                start_url: "./",
+                start_url: "/",
                 display: "standalone",
                 background_color: "#39c5bb",
                 icons: [
